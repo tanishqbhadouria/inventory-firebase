7f2eeb33505bb8b49cd487287782d7ca92f34e61
tanishqbhadouria <145770484+tanishqbhadouria@users.noreply.github.com>
2024-08-14 02:13:01 +0530

    Update firebase.js removed keys and ids

--- firebase.js
+++ firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBaYJaZAYnOPBdQEtismDLudLLNqM3_0gE",
-  authDomain: "inventory2-a6e99.firebaseapp.com",
-  projectId: "inventory2-a6e99",
-  storageBucket: "inventory2-a6e99.appspot.com",
-  messagingSenderId: "810407966825",
-  appId: "1:810407966825:web:93dce9bca5b468ab4dcaf8",
-  measurementId: "G-7W118T3Q5E"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
